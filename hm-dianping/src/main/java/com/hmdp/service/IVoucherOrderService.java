be0579af32e5424f13eccd33274d9fc7f365c104
snakexxx1 <2493492673@qq.COM>
2024-08-07 14:52:32 +0800

    添加优惠价并实现秒杀下单

--- hm-dianping/src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.VoucherOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -12,5 +13,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-12-22
  */
 public interface IVoucherOrderService extends IService<VoucherOrder> {
-
+    /**
+     * 实现下单业务
+     * @param voucherId
+     * @return
+     */
+    Result setKillVoucher(Long voucherId);
 }
